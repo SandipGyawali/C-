424abd59dc5f019e634da5ecbf7528402d7c5e6c
Sandip Gyawali <sandipgyawali10@gmail.com>
2023-01-26 18:15:39 +0545

    operator and dataype folder were added

---
+++ Operator/operator.c
@@ -0,0 +1,18 @@
+//Operator in C..
+/*
+Specific symbol which helps to perform logical and mathematical operation is known as operators.
+
+Types of Operators based on operands..
+1.Unary 
+2.Binary
+3.Ternary
+
+ Unary                          Binary         Ternary
+*(-)unary minus                *Arithmetic    *Conditional
+*++,-- ->increment,decrement   *Relational
+*!()->Logical Not              *Logical
+*Size of                       *Bitwise
+                               *Equality
+                               *Comma
+                               *Assignment
+*/
